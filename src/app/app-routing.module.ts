1946f664ab5d434cd72d75fba9da36323424c39c
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-11-27 18:35:08 -0800

    initial push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from '../app/app.component';
+import { ContactComponent } from './contact/contact.component';
+import { Heroes Component, HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HomeComponent } from './home/home.component';
 import { TodoComponent } from '../app/todo/todo.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'todo', component: TodoComponent }
+  { path: '', component: HomeComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: 'contact' component:  ContactComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'hero/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { TodoComponent } from './todo/todo.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
+  declarations: [
+    HomeComponent,
+    TodoComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
